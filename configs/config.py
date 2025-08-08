5938bb7df439cad466d6c4075f031d619f87d8dc
bblewitt1894 <bblewitt1894@gmail.com>
2025-08-08 05:18:52 +0100

    Added 2 new factions to the list.

--- configs/config.py
+++ configs/config.py
@@ -1,7 +1,7 @@
 CLASS_FLAGS = { "Warrior": 1, "Paladin": 2, "Hunter": 4, "Rogue": 8, "Priest": 16, "Shaman": 64, "Mage": 128, "Warlock": 256, "Druid": 1024 }
 
-ALLIANCE_FACTIONS = { 69, 54, 47, 72, 890, 730, 509, 1, 3, 4, 8 }
-HORDE_FACTIONS = { 530, 76, 81, 68, 729, 510, 889, 2, 5, 6, 9 }
+ALLIANCE_FACTIONS = { 69, 54, 47, 72, 890, 730, 509, 1, 3, 4, 8, 189 }
+HORDE_FACTIONS = { 530, 76, 81, 68, 729, 510, 889, 2, 5, 6, 9, 66 }
 
 PROGRESS_FILE = 'configs/quest_progress.json'
 SETTINGS_FILE = 'saves/settings.json'
